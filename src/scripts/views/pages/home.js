31391717bc3a97ff6d57e578b1bc14f346ca6e10
Langgggggggggg <147623270+Langgggggggggg@users.noreply.github.com>
2024-06-14 02:50:34 +0700

    Update home.js

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -4,7 +4,7 @@ import { createMakananItemTemplate } from "../templates/template-creator";
 const Home = {
   async render() {
     return `
-    <section class="flex justify-center tentang gap-36 mt-10">
+    <section class="flex justify-center tentang gap-36 my-56">
     <img class="logo" src="./images/heros/saborama.png" alt="web image">
     <div class="tentang">
         <div><p class="text-6xl text-[#ABA944]">SABORAMA</p></div>
@@ -21,7 +21,7 @@ const Home = {
 </section>
 
 
-<section class="flex justify-center mt-0 mb-15 jelajah">
+<section class="flex justify-center mt-0 mb-72 jelajah">
 <div class="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
 <div class="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
     <div class="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
