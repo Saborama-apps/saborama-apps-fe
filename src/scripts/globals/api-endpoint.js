0f56498142357b33d7a2aa593d1f7983b4e74df4
Saborama-apps <saboramaapps@gmail.com>
2024-06-15 00:07:45 +0700

    Update api-endpoint.js

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -4,7 +4,7 @@ const API_ENDPOINT = {
   HOME: `${CONFIG.BASE_URL}makanan`,
   DETAIL: (id) => `${CONFIG.BASE_URL}makanan/${id}`,
   DAERAH: `${CONFIG.BASE_URL}daerah`,
-  DETAILDAERAH: (IDBObjectStore) => `${CONFIG.BASE_URL}daerah/${IDBObjectStore}`,
+  DETAILDAERAH: (daerahId) => `${CONFIG.BASE_URL}daerah/${daerahId}`,
   ADD_REVIEW: `${CONFIG.BASE_URL}coment`,
 };
 
