f74a974cda0bd22634ab244e1818be87ae8276a3
thoriqarr21 <amuhammadthoriq@gmail.com>
2024-06-15 00:35:48 +0700

    Update jumbotron-hero.js

--- src/scripts/views/component/jumbotron-hero.js
+++ src/scripts/views/component/jumbotron-hero.js
@@ -7,7 +7,7 @@ class JumbotronHero extends HTMLElement {
      <source media="(max-width: 600px)" srcset="./images/heros/banner-small.jpg" >
       <div class="relative lg:h-[1000px] h-[300px] w-full text-center">
       <img  class="jumbotron absolute" src="./images/heros/banner-large.jpg" alt="web image">
-      <p class="tentang absolute text-white text-5xl w-full lg:top-[500px] top-[180px]">Selamat Datang Di <span class="text-[#ABA944]">SABORAMA</span><br>Sajian Budaya Lokal Melalui Kuliner</p>
+      <p class="tentang absolute text-white lg:text-5xl text-3xl w-full lg:top-[500px] top-[110px]">Selamat Datang Di <span class="text-[#ABA944]">SABORAMA</span><br>Sajian Budaya Lokal Melalui Kuliner</p>
       </div>
     </picture>
     </article>
